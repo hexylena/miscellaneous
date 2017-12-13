fff6c7eb1d124f0aa641b16ee9cf3f1925db2890
E Rasche <hxr@hx42.org>
2017-12-13 22:46:54 +0000

    remove home dir, preps

--- pronoun-practice/de.py
+++ pronoun-practice/de.py
@@ -1,7 +1,45 @@
 #!/usr/bin/env python3
+# encoding=utf-8
+import os
 import random
 import time
 
+LOGFILE = os.path.join(os.path.expanduser("~"), '.de_practice')
+
+prep = {
+    'akk': ['bis', 'durch', 'für', 'gegen', 'ohne', 'um', 'entlang'],
+    'dat': ['ab', 'aus', 'bei', 'mit', 'nach', 'seit', 'außer', 'von', 'zu'],
+    'wechselpräpositionen': ['an', 'auf', 'hinter', 'in', 'neben', 'über', 'unter', 'zwischen', 'vor']
+}
+
+prep_immer = {
+    'akk': {
+        'intressantent': 'für',
+        'ärger': 'über',
+        # 'gewoher': 'an',
+        'freuen': 'auf',
+        'warten': 'auf',
+    },
+    'dat': {
+        'anfangen': 'mit',
+        'einladen': 'mit',
+        'angst haben': 'vor',
+        'sich treffen': 'mit',
+        'helfen': 'bei',
+    }
+}
+
+
+trennbare_prefixes = [
+    # Mit
+    'an', 'auf', 'ab', 'zurück', 'vor', 'bei',
+    # Trenn das verb in zwei //
+    'los', 'weg', 'nach', 'zu', 'aus', 'ein',
+    # Bring ans End das muss sein
+    # Other ones that don't fit in the nice rhyme.
+    'fern', 'her', 'mit', 'statt', 'teil', 'um', 'weiter', 'zurecht',
+    'zusammen',
+]
 
 nouns = {
     'm': [('Man', 'Mann'), ('Bird', 'Vogel')],
@@ -68,8 +106,29 @@ case_names = {
     'gen': 'genative',
 }
 
+
 try:
-    if random.random() < 0.7:
+    if random.random() < 0.3:
+        case = random.choice(list(prep.keys()))
+        prep = random.choice(prep[case])
+
+        print("[prep] What case for %s" % prep)
+        asked = time.time()
+        expected = case
+        observed = input("Answer: ").strip()
+        answered = time.time()
+
+        if observed != expected[0:3]:
+            print("WRONG:", expected[0:3])
+        else:
+            if answered - asked < 5:
+                print("Acceptable.")
+            else:
+                print("Slow.")
+
+        with open(LOGFILE, 'a') as handle:
+            handle.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (asked, '_prep_', '', case, observed == expected[0:3], observed, expected[0:3], answered - asked))
+    elif random.random() < 0.7:
         defindef = random.choice(list(arts.keys()))
         (gender, case) = random.choice(list(arts[defindef]))
         noun_en, noun_de = random.choice(nouns[gender])
@@ -88,7 +147,7 @@ try:
             else:
                 print("Slow.")
 
-        with open('/home/hxr/.de_practice', 'a') as handle:
+        with open(LOGFILE, 'a') as handle:
             handle.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (asked, defindef, gender, case, observed == expected, observed, expected, answered - asked))
     else:
         pronoun = random.choice(list(pp.keys()))
@@ -107,7 +166,7 @@ try:
             else:
                 print("Slow.")
 
-        with open('/home/hxr/.de_practice', 'a') as handle:
+        with open(LOGFILE, 'a') as handle:
             handle.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (asked, '_pronoun_', pronoun, case, observed == expected, observed, expected, answered - asked))
 except KeyboardInterrupt:
     pass
